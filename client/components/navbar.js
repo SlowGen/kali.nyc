bad9d125a1fb635c05772f3f10482ab1bb6d44b3
Slowgen <kalibobailey@gmail.com>
2020-10-06 15:33:24 -0400

    Feat: added stylized buttons to nav bar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,10 +3,26 @@ import {Link} from 'react-router-dom'
 
 const Navbar = () => (
   <div>
-    <h1>SLOWGEN</h1>
+    <Link to="/home">
+      <img src="images/logo-nav.png" />
+    </Link>
     <nav>
       <div>
-        <Link to="/home">Home</Link>
+        <Link to="/home">
+          <img src="images/home.png" />
+        </Link>
+        <Link to="/about">
+          <img src="images/about.png" />
+        </Link>
+        <Link to="/work">
+          <img src="images/work.png" />
+        </Link>
+        <Link to="/photos">
+          <img src="images/photos.png" />
+        </Link>
+        <Link to="/msgme">
+          <img src="images/msgme.png" />
+        </Link>
       </div>
     </nav>
     <hr />
