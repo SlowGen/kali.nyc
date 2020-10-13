7dc09568a7f3e32f7c72dbd745bd33c07fcd9b73
Slowgen <kalibobailey@gmail.com>
2020-10-13 14:57:50 -0400

    Feat: reset msgme page back to links

---
+++ client/components/content/msgme-content-form.js
@@ -0,0 +1,61 @@
+/* eslint-disable no-alert */
+import React, {useState} from 'react'
+import {Box, Button} from 'rebass'
+import {Label, Input, Textarea} from '@rebass/forms'
+import axios from 'axios'
+
+const MsgMeContent = () => {
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const submitRequest = async e => {
+    e.preventDefault()
+    console.log({email, message})
+    try {
+      const response = await axios.post('/msgs', {
+        email,
+        message
+      })
+      console.log('response', response)
+
+      if (response.status === 200) {
+        alert('Message Sent.')
+        // e.resetForm()
+      } else if (response.status === 500) {
+        alert('Message failed to send.')
+      }
+    } catch (error) {
+      console.log('error with axios', error)
+    }
+  }
+
+  return (
+    <Box as="form" onSubmit={submitRequest}>
+      <Box>
+        <Label htmlFor="email">Email</Label>
+        <Input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="jane@example.com"
+          onChange={e => setEmail(e.target.value)}
+          value={email}
+        />
+      </Box>
+      <Box>
+        <Label htmlFor="message">Message</Label>
+        <Textarea
+          id="message"
+          name="message"
+          onChange={e => setMessage(e.target.value)}
+          value={message}
+        />
+      </Box>
+      <Box>
+        <Button>Submit</Button>
+      </Box>
+    </Box>
+  )
+}
+
+export default MsgMeContent
--- client/components/content/msgme-content.js
+++ client/components/content/msgme-content.js
@@ -1,62 +1,34 @@
 /* eslint-disable no-alert */
-import React, {useState} from 'react'
-import {Box, Button} from 'rebass'
-import {Label, Input, Textarea} from '@rebass/forms'
-import axios from 'axios'
+import React from 'react'
+import {Box, Flex, Image} from 'rebass'
 
 const MsgMeContent = () => {
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
-  const [sent, setSent] = useState(false)
-
-  const submitRequest = async e => {
-    e.preventDefault()
-    console.log({email, message})
-    try {
-      const response = await axios.post('/msgs', {
-        email,
-        message
-      })
-      console.log('response', response)
-
-      if (response.status === 200) {
-        alert('Message Sent.')
-        setSent(true)
-        // e.resetForm()
-      } else if (response.status === 500) {
-        alert('Message failed to send.')
-      }
-    } catch (error) {
-      console.log('error with axios', error)
-    }
-  }
-
   return (
-    <Box as="form" onSubmit={submitRequest}>
-      <Box>
-        <Label htmlFor="email">Email</Label>
-        <Input
-          id="email"
-          name="email"
-          type="email"
-          placeholder="jane@example.com"
-          onChange={e => setEmail(e.target.value)}
-          value={email}
-        />
+    <Flex justifyContent="space-evenly">
+      <Box justifyContent="center">
+        <a href="mailto:info@kali.nyc">
+          <Image src="images/email.png" />
+        </a>
       </Box>
-      <Box>
-        <Label htmlFor="message">Message</Label>
-        <Textarea
-          id="message"
-          name="message"
-          onChange={e => setMessage(e.target.value)}
-          value={message}
-        />
+      <Box justifyContent="center">
+        <a
+          href="https://www.linkedin.com/in/kali-nfn/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image src="images/linkedin.png" />
+        </a>
       </Box>
-      <Box>
-        <Button>Submit</Button>
+      <Box justifyContent="center">
+        <a
+          href="https://github.com/SlowGen"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image src="images/github.png" />
+        </a>
       </Box>
-    </Box>
+    </Flex>
   )
 }
 
